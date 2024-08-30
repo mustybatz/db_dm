81ca89a5977eca32e357d100587a927ec5262bb4
MENDEZ RUAN <is727844@iteso.mx>
2024-08-29 19:35:55 -0600

    feat: first commit, starting our first dimentional model

---
+++ restaurante_md.sql
@@ -0,0 +1,9 @@
+
+select * from DIM_ALIMENTO;
+
+---[DB].[schema].[tabla]
+insert into [Restaurante_md].[dbo].DIM_ALIMENTO (Id_alimento, nombre)
+select IdAlimento, Nombre from [Rest_Jr_sample].[dbo].ALIMENTO;
+
+
+
